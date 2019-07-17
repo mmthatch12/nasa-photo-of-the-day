5d15e50513ac0db679f51393d71b4782e22ee7db
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-07-17 13:57:24 -0600

    commented out url I will use

--- src/Components/Body/Photo.js
+++ src/Components/Body/Photo.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 
 import axios from 'axios';
 
+//https://api.nasa.gov/planetary/apod?api_key=keyqpOTD10PDmB3NlCQvuBgMs4qlPUd911rMfedh&date=2019-07-12
+
 function Photo() {
     const[img, setImg] = useState([]);
 
