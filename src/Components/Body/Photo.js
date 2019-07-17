88719ba5baf621818917073c49bb727f8723e61a
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-07-17 15:39:50 -0600

    hooked Title.js into Photo.js

--- src/Components/Body/Photo.js
+++ src/Components/Body/Photo.js
@@ -1,24 +1,30 @@
 import React, { useState, useEffect } from "react";
-
 import axios from 'axios';
+import Title from './Title'
 
 // https://api.nasa.gov/planetary/apod?api_key=keyqpOTD10PDmB3NlCQvuBgMs4qlPUd911rMfedh&date=2019-07-12
 
 export default function Photo() {
     const[im, setImg] = useState('');
+    const[title, setTitle] = useState('');
 
     useEffect(() => {
         axios
             .get(`https://api.nasa.gov/planetary/apod?api_key=keyqpOTD10PDmB3NlCQvuBgMs4qlPUd911rMfedh&date=2019-07-12`)
             .then(response => {
                 const photofday = response.data.url;
+                const titleofday = response.data.title;
                 console.log('is the the thing?', photofday)
+                console.log('title', titleofday)
                 setImg(photofday)
+                setTitle(titleofday)
             })
         }, [])
 
     return (
+
         <div className="photoDay">
+            <Title title={title}/>
             <img className='theImg' src={im}/>
         </div>
     )
