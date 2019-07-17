70e8d5918bdb119f20ecc14b139d9909266ef6d0
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-07-17 14:03:46 -0600

    working on axios .get function

--- src/Components/Body/Photo.js
+++ src/Components/Body/Photo.js
@@ -2,12 +2,16 @@ import React, { useState, useEffect } from "react";
 
 import axios from 'axios';
 
-//https://api.nasa.gov/planetary/apod?api_key=keyqpOTD10PDmB3NlCQvuBgMs4qlPUd911rMfedh&date=2019-07-12
+// https://api.nasa.gov/planetary/apod?api_key=keyqpOTD10PDmB3NlCQvuBgMs4qlPUd911rMfedh&date=2019-07-12
 
-function Photo() {
-    const[img, setImg] = useState([]);
+export default function Photo() {
+    const[im, setImg] = useState([]);
 
     useEffect(() =>) {
-
-    }
+        axios
+            .get(`https://api.nasa.gov/planetary/apod?api_key=keyqpOTD10PDmB3NlCQvuBgMs4qlPUd911rMfedh&date=2019-07-12`)
+            .then(response => {
+                const photofday = response.data.url;
+            })
+        }
 }
