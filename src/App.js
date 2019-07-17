2b4aea1f8bf60c88223518790bb6762c9132c792
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-07-17 15:16:59 -0600

    hooked up the header to the app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from "react";
 import "./App.scss";
+import { Header } from './Components/Header/Header'
 import Photo from './Components/Body/Photo'
+
 
 function App() {
 
   return (
     <div className="App">
+      <Header />
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun 🚀!
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Header = () => {
+    return (
+        <div className='headerCont'>
+            <h1>Nasa Photo of The Day</h1>
+
+        </div>
+    )
+}
+
