cc42673fd44534b59967316d69f0b8e39120ae39
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-07-17 13:51:18 -0600

    installed axios

--- src/Components/Body/Photo.js
+++ src/Components/Body/Photo.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from "react";
 
+import axios from 'axios';
+
 function Photo() {
     const[img, setImg] = useState([]);
 
     useEffect(() =>) {
-        
+
     }
 }
